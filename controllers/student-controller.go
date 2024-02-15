21a0f425318a3a6c73807dc7883b91fcf4608c90
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2024-02-15 15:46:15 +0300

    updated controller

--- controllers/student-controller.go
+++ controllers/student-controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"munenendereba/sms-backend/db"
 	models "munenendereba/sms-backend/models"
 )
@@ -15,4 +16,42 @@ func GetStudents() ([]*models.Student, error) {
 	}
 
 	return students, nil
+}
+
+func GetStudent(admissionNumber string) (*models.Student, error) {
+	var student models.Student
+
+	res := db.DB.First(&student, "admissionNumber = ?", admissionNumber)
+	if res.RowsAffected == 0 {
+		return nil, fmt.Errorf("student with admission %s not found", admissionNumber)
+	}
+
+	return &student, nil
+}
+
+func CreateStudent(student *models.Student) (*models.Student, error) {
+	res := db.DB.Create(&student)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return student, nil
+}
+
+func UpdateStudent(student *models.Student) (*models.Student, error) {
+	res := db.DB.Save(student)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return student, nil
+}
+
+func DeleteStudent(admissionNumber string) error {
+	var deletedStudent models.Student
+
+	res := db.DB.Delete(&deletedStudent, "admissionNumber =?", admissionNumber)
+
+	if res.RowsAffected == 0 {
+		return fmt.Errorf("student with admission %s not found", admissionNumber)
+	}
+	return nil
 }
